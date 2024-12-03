3265fddc65fab61ecae87cb1fe4e017e9b61e54c
Andrea <andreaxtan611@gmail.com>
2024-12-03 19:30:12 +0800

    Update templates

--- server/templates/js/animation.js
+++ server/templates/js/animation.js
@@ -1,6 +1,15 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.to(".box", {
-        rotation: 360,
-        duration: 2
-    })
+gsap.registerPlugin(ScrollTrigger);
+
+let bodyScrollBar = Scrollbar.init(document.body, {
+    damping: 0.1,
+    delegateTo: document,
 });
+ScrollTrigger.scrollerProxy(".scroller", {
+    scrollTop(value) {
+        if (arguments.length) {
+            bodyScrollBar.scrollTop = value;
+        }
+        return bodyScrollBar.scrollTop;
+    },
+});
+bodyScrollBar.addListener(ScrollTrigger.update);
