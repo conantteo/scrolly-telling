0aff57ba4912cddbd864b1d28a223fda5b3fbee4
unknown <conantteo95@gmail.com>
2024-12-04 18:05:42 +0800

    added FormInput and RadioGroup to component drawer

--- frontend/src/components/ComponentDrawer.tsx
+++ frontend/src/components/ComponentDrawer.tsx
@@ -1,16 +1,19 @@
-import { Box, Button, Drawer, Stack, Title } from '@mantine/core';
+import { useState } from 'react';
+import { Box, Button, Drawer, FileInput, Group, Radio, Stack, Title } from '@mantine/core';
 import { useScrollyStore } from '../store';
+import { ScrollyElementData } from '../types';
+
+const ALLOW_EXTENSIONS = ['png', 'jpg', 'jpeg'];
 
 const ComponentDrawer: React.FC = () => {
   const isComponentWindowOpen = useScrollyStore((state) => state.isComponentWindowOpen);
   const currentElementId = useScrollyStore((state) => state.currentElementId);
-
   const upsertData = useScrollyStore((state) => state.upsertData);
   const upsertElement = useScrollyStore((state) => state.upsertElement);
   const setViewElement = useScrollyStore((state) => state.setViewElement);
-
   const elements = useScrollyStore((state) => state.elements);
   const data = useScrollyStore((state) => state.data);
+
   const targetElement = elements.find((element) => element.id === currentElementId) ?? {
     id: currentElementId,
     type: 'component',
@@ -22,8 +25,39 @@ const ComponentDrawer: React.FC = () => {
     type: 'component',
     metadata: {},
   };
+  const [modifiedData, setModifiedData] = useState<ScrollyElementData>({ ...targetData });
+  const [formError, setFormError] = useState({ type: '', file: '' });
+
   const onClose = () => {
     setViewElement(currentElementId, false);
+  };
+  const onFileUpload = (file: File | null) => {
+    if (!file) {
+      return;
+    }
+    const fileExtension = file.name.split('.').pop();
+    if (fileExtension && ALLOW_EXTENSIONS.includes(fileExtension?.toLowerCase())) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const base64 = reader.result as string;
+        setModifiedData({
+          ...modifiedData,
+          metadata: {
+            ...modifiedData.metadata,
+            fileBase64: base64,
+            fileName: file.name,
+            fileExtension,
+            fileSize: `${file.size}`,
+          },
+        });
+      };
+    } else {
+      setFormError((prev) => ({
+        ...prev,
+        file: `Only the following file extensions are allowed: ${ALLOW_EXTENSIONS.join(', ')}`,
+      }));
+    }
   };
   return (
     <Drawer opened={isComponentWindowOpen} onClose={onClose} position="right" size="xl">
@@ -31,12 +65,47 @@ const ComponentDrawer: React.FC = () => {
         <Title order={2}>
           {targetElement.isNew ? `Create new component` : `Edit component ${currentElementId}`}
         </Title>
-        <Box>Component Content</Box>
+        <Box>
+          <Radio.Group
+            value={modifiedData.metadata.type}
+            onChange={(value) =>
+              setModifiedData((prev) => ({
+                ...prev,
+                metadata: {
+                  ...prev.metadata,
+                  type: value,
+                },
+              }))
+            }
+            label="Choose a component to create"
+            description="Select either an image or text component"
+          >
+            <Group mt="xs">
+              <Radio label="Image" value="image" />
+              <Radio label="Text" value="text" />
+            </Group>
+          </Radio.Group>
+        </Box>
+        {modifiedData.metadata.type === 'image' && (
+          <Box>
+            <FileInput
+              radius="xl"
+              label="Upload Image"
+              withAsterisk
+              description=".png"
+              error={formError.file ? formError.file : null}
+              placeholder={
+                modifiedData.metadata.fileName ? modifiedData.metadata.fileName : 'Select an image'
+              }
+              onChange={onFileUpload}
+            />
+          </Box>
+        )}
         <Box style={{ position: 'fixed', bottom: 0, right: 0, padding: '12px' }}>
           <Button
             onClick={() => {
               onClose();
-              upsertData(targetData);
+              upsertData(modifiedData);
               upsertElement(targetElement);
             }}
           >
--- frontend/src/components/ScrollyContainer.tsx
+++ frontend/src/components/ScrollyContainer.tsx
@@ -15,7 +15,9 @@ const ScrollyContainer: React.FC = () => {
       <Stack align="stretch" justify="center" gap="xs">
         {viewableElements.map((element) => (
           <Card key={element.id} withBorder shadow="xl">
-            <div> {element.type == "component" ? "Component" : "Animation"} {element.id}</div>
+            <div>
+              {element.type === 'component' ? 'Component' : 'Animation'} {element.id}
+            </div>
           </Card>
         ))}
       </Stack>
