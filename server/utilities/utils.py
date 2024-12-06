9953c67e5abc8b6e077d1373a2394e800af66006
shelleyyap <yapyingying94@gmail.com>
2024-12-06 23:05:58 +0800

    giving images uploaded unique filenames by using uuid

--- server/utilities/utils.py
+++ server/utilities/utils.py
@@ -2,6 +2,7 @@ import logging
 import os
 from pathlib import Path
 from typing import Any
+from uuid import uuid4
 
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
@@ -29,13 +30,14 @@ def generate_js_function(template_path: Path, output_file: Path, **kwargs: Any) 
     logger.info(f"JavaScript function generated in {output_file}")
 
 def stage_file(minio_client, article_id: str, file_content: bytes, filename: str, file_size: int, file_content_type: str) -> None:
+    new_filename = f"{str(uuid4())}{Path(filename).suffix}"
     if IS_LOCAL:
         Path.mkdir(LOCAL_OUTPUT_DIR / article_id / "images", parents=True, exist_ok=True)
-        with open(LOCAL_OUTPUT_DIR / article_id / "images" / filename, "wb") as f:
+        with open(LOCAL_OUTPUT_DIR / article_id / "images" / new_filename, "wb") as f:
             f.write(file_content.read())
-        return LOCAL_OUTPUT_DIR / article_id / "images" / filename
+        return LOCAL_OUTPUT_DIR / article_id / "images" / new_filename
     else:
         if not minio_client.bucket_exists(MINIO_ARTICLE_BUCKET):
             minio_client.make_bucket(MINIO_ARTICLE_BUCKET)
-        minio_client.put_object(MINIO_ARTICLE_BUCKET, f"{article_id}/images/{filename}", file_content, length=file_size, content_type=file_content_type)
-        return f"{MINIO_ENDPOINT.replace('minio', 'localhost')}/{MINIO_ARTICLE_BUCKET}/{article_id}/images/{filename}"
+        minio_client.put_object(MINIO_ARTICLE_BUCKET, f"{article_id}/images/{new_filename}", file_content, length=file_size, content_type=file_content_type)
+        return f"{MINIO_ENDPOINT.replace('minio', 'localhost')}/{MINIO_ARTICLE_BUCKET}/{article_id}/images/{new_filename}"
